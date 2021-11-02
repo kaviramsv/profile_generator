ece58fee5ed59e34b6c316cf2ba10e3348c7996c
Kavita Ramesh <2706051+kaviramsv@users.noreply.github.com>
2021-11-02 15:44:17 -0400

    Update survey.js

--- survey.js
+++ survey.js
@@ -6,16 +6,17 @@ const rl = readline.createInterface({
 });
 
 rl.question("What's your name? Nicknames are also acceptable :)", (name) => {
-  //console.log(`Thank you for your valuable feedback: ${answer}`);
+  
 rl.question("What's an activity you like doing?", (activity) => {
-  //console.log(`Thank you for your valuable feedback: ${name} and ${activity} `);
+  
      rl.question("What's your fav food", (food) => {
-    //  // console.log(`Thank you for your valuable feedback: ${answer}`);
+    
        rl.question("What's your fav color)", (color) => {
-       //console.log(`Thank you for your valuable feedback: ${answer}`);
+    
        rl.question("Whhich is your fav place", (place) => {
-          console.log(` Your name is ${name}fav activity is ${activity} and u love to eat ${food}.You love ${color} color.You think ${place} is a good place to hang out`);
+         
+          console.log(` Your name is ${name}fav activity is ${activity} and 
+          u love to eat ${food}.You love ${color} color.You think ${place} is a good place to hang out`);
 
           rl.close();
 })})})})});
-// }) });
